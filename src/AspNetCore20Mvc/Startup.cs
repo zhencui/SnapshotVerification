c57c0954434c68b7eab7bc1e406695d73cc929a7
Wuyi Weng <wuweng@microsoft.com>
2017-10-18 20:04:02 -0700

    - add snapshot

--- src/AspNetCore20Mvc/Startup.cs
+++ src/AspNetCore20Mvc/Startup.cs
@@ -6,11 +6,28 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.ApplicationInsights.AspNetCore;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.SnapshotCollector;
 
 namespace AspNetCore20Mvc
 {
     public class Startup
     {
+        private class SnapshotCollectorTelemetryProcessorFactory : ITelemetryProcessorFactory
+        {
+            public ITelemetryProcessor Create(ITelemetryProcessor next)
+            {
+                SnapshotCollectorConfiguration configuration = new SnapshotCollectorConfiguration()
+                {
+                    ThresholdForSnapshotting = 1,   // How many times we need to see an exception before we ask for snapshots. Default is 5. The value cannot be less than 1.
+                    MaximumSnapshotsRequired = 5,   //The maximum number of snapshots we collect for a single problem. Default is 3. The value must be between 1 and 999.
+                    SnapshotsPerDayLimit = 0      // The maximum number of snapshots allowed in one day (24 hours). Default is 50. 0 means not limit. The limit must not be negative.
+                };
+                return new SnapshotCollectorTelemetryProcessor(next, configuration);
+            }
+        }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -21,6 +38,7 @@ namespace AspNetCore20Mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ITelemetryProcessorFactory>(new SnapshotCollectorTelemetryProcessorFactory());
             services.AddMvc();
         }
 
