2f5803f4d1f9a5f9b842312bcd994a628d788ed0
Wuyi Weng <wuweng@microsoft.com>
2017-10-25 22:00:10 -0700

    - update package

---
+++ src/AspNetCore20MvcEastUS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCore20MvcEastUS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ src/AspNetCore20MvcEastUS/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.ApplicationInsights;
+
+namespace AspNetCore20MvcEastUS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
+                .UseStartup<Startup>()
+                .Build();
+
+            TelemetryClient client = new TelemetryClient();
+            // This is used for detect site restart.
+            client.TrackEvent("SiteStart");
+            client.Flush();
+
+            return host;
+        }
+    }
+}
