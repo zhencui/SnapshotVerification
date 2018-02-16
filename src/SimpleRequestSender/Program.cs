544c0fc746fde0e03d55c9a9833bb910740e60a5
Wuyi Weng <wuweng@microsoft.com>
2018-02-15 16:20:23 -0800

    - change to public nuget

--- src/SimpleRequestSender/Program.cs
+++ src/SimpleRequestSender/Program.cs
@@ -13,14 +13,24 @@ namespace SimpleRequestSender
             // Change this to the url you want to ping.
             List<string> urls = new List<string>
             {
-                "http://auto-light-up-asp-net46-old.azurewebsites.net/Home/About",
-                "http://auto-light-up-asp-net46-old.azurewebsites.net/Home/Contact",
-                "http://auto-light-up-asp-net-core20-old.azurewebsites.net/Home/About",
-                "http://auto-light-up-asp-net-core20-old.azurewebsites.net/Home/Contact",
-                "http://auto-light-up-asp-net46.azurewebsites.net/Home/About",
-                "http://auto-light-up-asp-net46.azurewebsites.net/Home/Contact",
-                "http://auto-light-up-asp-net-core20.azurewebsites.net/Home/About",
-                "http://auto-light-up-asp-net-core20.azurewebsites.net/Home/Contact"
+                //"http://auto-light-up-asp-net46-old.azurewebsites.net/Home/About",
+                //"http://auto-light-up-asp-net46-old.azurewebsites.net/Home/Contact",
+                //"http://auto-light-up-asp-net-core20-old.azurewebsites.net/Home/About",
+                //"http://auto-light-up-asp-net-core20-old.azurewebsites.net/Home/Contact",
+                //"http://auto-light-up-asp-net46.azurewebsites.net/Home/About",
+                //"http://auto-light-up-asp-net46.azurewebsites.net/Home/Contact",
+                //"http://auto-light-up-asp-net-core20.azurewebsites.net/Home/About",
+                //"http://auto-light-up-asp-net-core20.azurewebsites.net/Home/Contact",
+                //"http://auto-ena-restart.azurewebsites.net/Home/About",
+                //"http://auto-ena-restart.azurewebsites.net/Home/Contact"
+                "http://nuget-12-restart-46-1.azurewebsites.net/Home/About",
+                "http://nuget-12-restart-46-1.azurewebsites.net/Home/Contact",
+                "http://nuget-12-restart-46-2.azurewebsites.net/Home/About",
+                "http://nuget-12-restart-46-2.azurewebsites.net/Home/Contact",
+                "http://nuget-12-restart-core20-1.azurewebsites.net/Home/About",
+                "http://nuget-12-restart-core20-1.azurewebsites.net/Home/Contact",
+                "http://nuget-12-restart-core20-2.azurewebsites.net/Home/About",
+                "http://nuget-12-restart-core20-2.azurewebsites.net/Home/Contact"
             };
 
             using (var sender = new RequestSender(urls))
