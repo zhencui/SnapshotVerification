2907da50d7391265fb46bd263451d041e84679b5
Wuyi Weng <wuweng@microsoft.com>
2018-02-23 14:51:24 -0800

    - add sf app

---
+++ src/cd_e2e_sf_web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cd_e2e_sf_web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
