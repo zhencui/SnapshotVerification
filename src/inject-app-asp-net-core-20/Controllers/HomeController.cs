e6e22f74e335f69e99bd24119c96a48154307199
Wuyi Weng <wuweng@microsoft.com>
2018-02-26 10:05:11 -0800

    - add inject app

---
+++ src/inject-app-asp-net-core-20/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using inject_app_asp_net_core_20.Models;
+using SnapshotTest;
+
+namespace inject_app_asp_net_core_20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            CPUIntensiveComputation.RecusiveCall1(12);
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/inject-app-asp-net-core-20/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace inject_app_asp_net_core_20.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
